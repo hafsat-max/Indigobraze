7393a69b1f464394d02f41d2e99f888d39b4d2aa
Abiodun Busari <hafsahbusari69@gmail.com>
2024-05-17 11:27:44 +0100

    Feat: all client"s requested updates

--- src/components/newsletter.tsx
+++ src/components/newsletter.tsx
@@ -5,9 +5,9 @@ const Newsletter = () => {
   return (
     <section className='bg-[#F4F5F6] py-20 flex flex-col justify-center items-center px-4'>
        <div className='px-[18px] flex flex-col gap-8'>
-        <div className='flex flex-col'>
+        <div className='flex flex-col gap-2'>
             <h3 className='text-center text-[#292929] text-5xl'>Subscribe to our newsletter </h3>
-            <h5 className='text-[#292929] text-2xl text-center'>to get updates for newest property</h5>
+            <h5 className='text-[#292929] text-2xl text-center'>to get newest updates.</h5>
         </div>
         <div className='items-center flex justify-between w-full gap-4'>
             <input className='border border-[#6E5DCF] px-10 py-[5px] rounded-lg'/>
--- src/components/tetimonial-item.tsx
+++ src/components/tetimonial-item.tsx
@@ -3,29 +3,38 @@ import Image, { StaticImageData } from "next/image";
 
 // TestimonialItem.tsx
 interface ITestimonialItemProps {
-    testimonial: string;
-    name: string;
-    isActive: boolean;
-    image: string;
+  testimonial: string;
+  name: string;
 }
-export const TestimonialItem = ({ testimonial, name, isActive, image }:ITestimonialItemProps) => {
-    const activeClass = "scale-100 bg-primary text-white h-[400px]";
-    const normalClass = "scale-90 bg-white h-[380px]";
+export const TestimonialItem = ({
+  testimonial,
+  name,
+}: ITestimonialItemProps) => {5
+  return (
+    <div
+      className={`cursor-grab transition-transform duration-300 shadow-lg rounded-lg overflow-hidden mb-4 border flex flex-col bg-primary w-[60%] h-[600px] mx-auto `}
+    >
+      <div
+        className={` bg-cover bg-no-repeat bg-center flex-1 `}
+        style={{
+          backgroundImage: `url(/ibroheem.png)`,
+        }}
+      >
+      </div>
 
-    return (
-        <div className={`cursor-grab transition-transform duration-300 shadow-lg rounded-lg overflow-hidden mb-4 border flex flex-col ${isActive ? activeClass : normalClass}`}>
-            <div className={` bg-cover bg-no-repeat flex-1`}
-            style={{
-                backgroundImage:`url(${image})`
-            }}
-            >
-
-            </div>
-
-            <div className="py-6 px-4 flex flex-col">
-            <p className={`font-bold  text-2xl ${isActive? 'text-white line-clamp-10 md:line-clamp-12': 'text-[#292929]  line-clamp-8 md:line-clamp-10'}`}>{name}</p>
-            <p className={`${isActive? 'text-white line-clamp-10 md:line-clamp-12': 'text-gray-600 line-clamp-8 md:line-clamp-10'}`}>{testimonial}</p>
-            </div>
-        </div>
-    );
+      <div className="py-6 px-4 flex flex-col">
+        <p
+          className={`font-bold  text-2xl  'text-white line-clamp-10 md:line-clamp-12 text-white`}
+        >
+          {name}
+        </p>
+        <p className={` text-white line-clamp-10 md:line-clamp-12' }`}>
+          {testimonial}
+        </p>
+      </div>
+    </div>
+  );
 };
+
+// : 'text-[#292929]  line-clamp-8 md:line-clamp-10'}
+// 'text-gray-600 line-clamp-8 md:line-clamp-10
